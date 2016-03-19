0795e86b8b344e907bc9e420203b0edecc70016b
Ryan Price <rsprice90@gmail.com>
2016-03-19 04:11:40 -0700

    (feat) add basic webpack config, index.html, and index.js files

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+let path    = require('path');
+let webpack = require('webpack');
+
+module.exports = {
+  entry: ['./index'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ]
+};
